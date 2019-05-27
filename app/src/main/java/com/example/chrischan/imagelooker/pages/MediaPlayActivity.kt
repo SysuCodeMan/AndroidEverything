1ef5d5b5ca52a8ec8f2eebef087f4f6d9a82a39c
Lucifer <chennengshi@meituan.com>
2019-05-27 17:51:07 +0800

    finish menu

--- app/src/main/java/com/example/chrischan/imagelooker/pages/MediaPlayActivity.kt
+++ app/src/main/java/com/example/chrischan/imagelooker/pages/MediaPlayActivity.kt
@@ -1,7 +1,6 @@
 package com.example.chrischan.imagelooker.pages
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import com.example.chrischan.imagelooker.R
 
